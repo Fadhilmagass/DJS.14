5188137df89eafcd9883abd9e1574542bc3decd8
Fadhil Muhammad Agast <git-agas.fadhil@gmail.com>
2023-02-13 06:11:06 +0700

    add Procfile

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -6,16 +6,13 @@ module.exports = {
     name: "ready",
     once: true,
     async execute(client, guild) {
-        mongoose.connect(config.mongodb || '', {
+        await mongoose.connect(config.mongodb || '', {
             keepAlive: true,
-        }).catch((error) => {
-            console.error("Error connecting to MongoDB:", error);
-        });
+        })
 
         if (mongoose.connect) {
             console.log("MongoDB connection successful | ✅");
         }
-
         console.log(`${client.user.username} is now Online 🚀`);
 
         client.user.setPresence({
